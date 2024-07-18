ccc82c791364c34a4c9d428a88daaad389befe36
Abhinav Raj <abhiraj61002@gmail.com>
2024-07-18 19:19:59 +0530

    EDIT

--- Programs/JAVA Programs/JavaComboBox.java
+++ Programs/JAVA Programs/JavaComboBox.java
@@ -7,8 +7,12 @@ public class JavaComboBox
         jf = new JFrame("Combo Box Example");
         String country[] = {"India", "USA", "Russia","China"};
         JComboBox cb = new JComboBox(country);
+        String capital[] = {"Delhi","Washington D.C.","Moscow","Beijing"};
+        JComboBox cb1 = new JComboBox(capital);
         cb.setBounds(50,50,100,20);
+        cb1.setBounds(50,100,100,20);
         jf.add(cb);
+        jf.add(cb1);
         jf.setLayout(null);
         jf.setSize(400,500);
         jf.setVisible(true);
